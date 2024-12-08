d2a4d86f3f7072991784a30c490828c5a04c5bfa
Mariluz2024 <mluzco29@gmail.com>
2024-12-08 16:19:03 +0100

    Plantilla de ítem de tarea

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ListItem = ({ task }) => {
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      <span>
+        {task.title}{" "}
+        {task.isCompleted ? (
+          <span className="text-success">✔️</span>
+        ) : (
+          <span className="text-danger">❌</span>
+        )}
+      </span>
+      <button className="btn btn-danger btn-sm">
+        Eliminar
+      </button>
+    </li>
+  );
+};
+
+export default ListItem;
